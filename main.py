917aa1ff08da6eba3d61d27b6d79caa452e03476
A1rb0und3r1n0 <diekmann.johannes@web.de>
2025-06-10 21:46:56 +0200

    added respawn screen

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ def main():
     score_font = pygame.font.Font(None, 36)
     pause_font = pygame.font.Font(None, 128)
 
+    
     score = 0
 
     updatables = pygame.sprite.Group()
@@ -52,22 +53,34 @@ def main():
 
     while True:
         game_state()
+
+        global PLAYER_ALIVE
         
         screen.fill("black")
         score_text = score_font.render(f'Score: {score}', True, (255, 255, 255))
         pause_text = pause_font.render('PAUSED', True, (255, 255, 255))
+        death_text = pause_font.render('GAME OVER', True, (255, 255, 255))
+        lives_text = pause_font.render(f'Lives remaining: {PLAYER_LIVES}', True, (255, 255, 255))
+        respawn_text = score_font.render('PRESS SPACE TO RESPAWN', True, (255, 255, 255))
         screen.blit(score_text, (10, 10))
+        screen.blit(lives_text, (SCREEN_WIDTH, 10))
 
         if PAUSE:
             screen.blit(pause_text,(SCREEN_WIDTH/2 - pause_text.get_width()/2, SCREEN_HEIGHT/2 - pause_text.get_height()/2))
+        elif not PLAYER_ALIVE:
+            if PLAYER_LIVES > 0:
+                screen.blit(lives_text,(SCREEN_WIDTH/2 - lives_text.get_width()/2, SCREEN_HEIGHT/2 - lives_text.get_height()/2))
+                screen.blit(respawn_text,(SCREEN_WIDTH/2 - respawn_text.get_width()/2, SCREEN_HEIGHT/2 - respawn_text.get_height()/2 + lives_text.get_height()))
+            else:
+                screen.blit(death_text,(SCREEN_WIDTH/2 - death_text.get_width()/2, SCREEN_HEIGHT/2 - death_text.get_height()/2))
         else:
-
             updatables.update(dt)
 
             for asteroid in asteroids:
                 if player.colliding_with(asteroid):
                     print("Game Over!")
-                    return
+                    PLAYER_ALIVE = False
+                    #return
             
             for asteroid in asteroids:
                 for shot in shots:
